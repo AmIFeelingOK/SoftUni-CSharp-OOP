9b1d7e3fd0764452af332958b0d838140581cd42
Peter IT <97403570+AmIFeelingOK@users.noreply.github.com>
2022-07-08 10:23:29 +0300

    Add files via upload

---
+++ 01.Inheritance-Lab/04.RandomList/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList randomList = new RandomList();
+
+            randomList.Add("One");
+            randomList.Add("Two");
+            randomList.Add("Three");
+            randomList.Add("Four");
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(randomList.RandomString());
+            }
+        }
+    }
+}
