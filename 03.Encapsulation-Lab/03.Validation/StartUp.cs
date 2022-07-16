6f7758115e126b499026b03bfa83c114edd1e797
Peter IT <97403570+AmIFeelingOK@users.noreply.github.com>
2022-07-16 13:18:12 +0300

    Add files via upload

---
+++ 03.Encapsulation-Lab/03.Validation/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            List<Person> people = new List<Person>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                var cmdArgs = Console.ReadLine().Split();
+                var person = new Person(cmdArgs[0]
+                    , cmdArgs[1]
+                    , int.Parse(cmdArgs[2])
+                    , decimal.Parse(cmdArgs[3]));
+
+                people.Add(person);
+            }
+            var parcentage = decimal.Parse(Console.ReadLine());
+            people.ForEach(x => x.IncreaseSalary(parcentage));
+            people.ForEach(x => Console.WriteLine(x.ToString()));
+
+        }
+    }
+}
---
+++ 03.Encapsulation-Lab/04.Team/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            List<Person> people = new List<Person>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                var cmdArgs = Console.ReadLine().Split();
+                var person = new Person(cmdArgs[0]
+                    , cmdArgs[1]
+                    , int.Parse(cmdArgs[2])
+                    , decimal.Parse(cmdArgs[3]));
+
+                people.Add(person);
+            }
+            Team team = new Team("DreamTeam");
+
+            people.ForEach(x => team.AddPlayer(x));
+            Console.WriteLine($"First Team: {team.FirstTeam.Count}");
+            Console.WriteLine($"Reserve Team: {team.ReserveTeam.Count}");
+        }
+    }
+}
