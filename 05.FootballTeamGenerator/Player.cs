f0b2ab76f7e2ea498bd9ed97889ee3983f2d892f
Peter IT <97403570+AmIFeelingOK@users.noreply.github.com>
2022-07-19 15:35:13 +0300

    Add files via upload

---
+++ 05.FootballTeamGenerator/Player.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _05.FootballTeamGenerator
+{
+    public class Player
+    {
+        private string name;
+
+        public Player(string name, Stats stats)
+        {
+            this.Name = name;
+            this.Stats = stats;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            private set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ErrorMessages.NullOrWhitespaceName);
+                }
+
+                this.name = value;
+            }
+        }
+
+        public Stats Stats { get; private set; }
+    }
+}
