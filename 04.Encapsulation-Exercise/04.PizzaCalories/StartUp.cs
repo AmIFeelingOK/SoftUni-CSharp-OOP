263a6a5c6cbca4c1fd0c205c026c7afa328b0221
Peter IT <97403570+AmIFeelingOK@users.noreply.github.com>
2022-07-19 07:46:43 +0300

    Add files via upload

---
+++ 04.Encapsulation-Exercise/04.PizzaCalories/StartUp.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _04.PizzaCalories
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string[] pizzaInfo = Console.ReadLine().Split();
+                Pizza pizza = new Pizza(pizzaInfo[1]);
+
+                string[] doughInfo = Console.ReadLine().Split();
+                Dough dough = new Dough(doughInfo[1].ToLower(), doughInfo[2].ToLower(), double.Parse(doughInfo[3]));
+
+                pizza.PizzaDough = dough;
+
+                string toppingInfo;
+
+                while ((toppingInfo = Console.ReadLine()) != "END")
+                {
+
+                    string[] toppingParams = toppingInfo.Split();
+
+                    string toppingName = toppingParams[1].ToLower();
+                    double toppingWeight = double.Parse(toppingParams[2]);
+                    Topping topping = new Topping(toppingName, toppingWeight);
+
+                    pizza.Add(topping);
+
+                }
+
+                Console.WriteLine($"{pizza.Name} - {pizza.PizzaCalories():f2} Calories.");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
+
