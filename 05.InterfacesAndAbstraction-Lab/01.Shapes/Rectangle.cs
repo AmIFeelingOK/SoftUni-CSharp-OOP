5b36e625088710232909b403411deb160794c647
Peter IT <97403570+AmIFeelingOK@users.noreply.github.com>
2022-07-20 18:34:45 +0300

    Add files via upload

---
+++ 05.InterfacesAndAbstraction-Lab/01.Shapes/Rectangle.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : IDrawable
+    {
+        private int height;
+        private int width;
+
+        public Rectangle(int height, int width)
+        {
+            this.Height = height;
+            this.Width = width;
+        }
+
+        public int Height 
+        {
+            get
+            {
+                return this.height;
+            }
+            set
+            {
+                this.height = value;
+            }
+        }
+        public int Width 
+        {
+            get
+            {
+                return this.width;
+            }
+            set
+            {
+                this.width = value;
+            }
+        }
+
+        public void Draw()
+        {
+            for (int i = 0; i < this.Height; i++)
+            {
+                for (int j = 0; j < this.Width; j++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 05.InterfacesAndAbstraction-Lab/01.Shapes/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int height = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+            int radius = int.Parse(Console.ReadLine());
+
+            IDrawable rect = new Rectangle(height, width);
+            IDrawable circle = new Circle(radius);
+
+            rect.Draw();
+            circle.Draw();
+        }
+    }
+}
